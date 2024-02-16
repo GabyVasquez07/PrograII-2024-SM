7adc41cda38066cc7c5eb685b3fc74b530269b5d
GabyVasquez07 <gabrielaestefanivasquez@gmail.com>
2024-02-15 21:44:46 -0600

    Se creo el conversor de longitud

--- app/src/main/java/com/ugb/controlesbasicos/MainActivity.java
+++ app/src/main/java/com/ugb/controlesbasicos/MainActivity.java
@@ -17,6 +17,8 @@ public class MainActivity extends AppCompatActivity {
     Spinner spn;
     Button btn;
     conversores miObj = new conversores();
+    tiempo miti = new conversores();
+    alma miObj2 = new alma();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,6 +29,10 @@ public class MainActivity extends AppCompatActivity {
         tbh.addTab(tbh.newTabSpec("LON").setIndicator("LONGITUD", null).setContent(R.id.tabLongitud));
         tbh.addTab(tbh.newTabSpec("MON").setIndicator("MONEDAS", null).setContent(R.id.tabMonedas));
         tbh.addTab(tbh.newTabSpec("ALM").setIndicator("ALMACENAMIENTO", null).setContent(R.id.tabAlmacenamiento));
+        tbh.addTab(tbh.newTabSpec("MAS").setIndicator("MASA",null).setContent(R.id.tabMasa));
+        tbh.addTab(tbh.newTabSpec("TIE").setIndicator("TIEMPO",null).setContent(R.id.tabTiempo));
+        tbh.addTab(tbh.newTabSpec("VOL").setIndicator("VOLUMEN",null).setContent(R.id.tabTrasferenciaDatos));
+        tbh.addTab(tbh.newTabSpec("TRANS").setIndicator("TRANFERENCIA DE DATOS",null).setContent(R.id.tabTrasferenciaDatos));
 
         btn = findViewById(R.id.btnConvertirLongitud);
         btn.setOnClickListener(new View.OnClickListener() {
@@ -46,6 +52,25 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(getApplicationContext(), "Respuesta: "+ resp, Toast.LENGTH_LONG).show();
             }
         });
+
+        btn = findViewById(R.id.btnConvertirAlma);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                spn = findViewById(R.id.spnDeAlma);
+                int de = spn.getSelectedItemPosition();
+
+                spn = findViewById(R.id.spnAAlma);
+                int a = spn.getSelectedItemPosition();
+
+                tempVal = findViewById(R.id.txtCantidadAlma);
+                double cantidad= Double.parseDouble(tempVal.getText().toString());
+
+                double resp = miObj2.convertir2(0, de, a, cantidad);
+
+                Toast.makeText(getApplicationContext(), "Respuesta: "+ resp, Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
 class conversores{
@@ -54,5 +79,21 @@ class conversores{
     };
     public double convertir(int opcion, int de, int a, double cantidad){
         return valores[opcion][a] / valores[opcion][de] * cantidad;
+    }
+}
+class alma{
+    double[][] valores2 = {
+            {1, 8, 8192, 8388608, 1048576, 1073741824}
+    };
+    public double convertir2(int opcion, int de, int a, double cantidad){
+        return valores2[opcion][a] / valores2[opcion][de] * cantidad;
+    }
+}
+class tiempo{
+    double[][] valores2 = {
+            {1, 8, 8192, 8388608, 1048576, 1073741824}
+    };
+    public double convertir2(int opcion, int de, int a, double cantidad){
+        return valores2[opcion][a] / valores2[opcion][de] * cantidad;
     }
 }
