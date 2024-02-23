bb6720f1a735fc23c05e3d5674a0d566b29053ac
GabyVasquez07 <gabrielaestefanivasquez@gmail.com>
2024-02-22 20:17:28 -0600

    sensor acelerometro

--- app/src/main/java/com/ugb/controlesbasicos/MainActivity.java
+++ app/src/main/java/com/ugb/controlesbasicos/MainActivity.java
@@ -14,15 +14,56 @@ import android.widget.TabHost;
 import android.widget.TextView;
 import android.widget.Toast;
 import android.util.Log;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 public class MainActivity extends AppCompatActivity {
-
+    SensorManager sensorManager;
+    Sensor sensor;
+    SensorEventListener sensorEventListener;
+    TextView tempVal;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
-        };
-
+        tempVal = findViewById(R.id.lblSensorAcelerometro);
+        acvtivarSensorAcelerometro();
     }
+    @Override
+    protected void onResume() {
+        inicar();
+        super.onResume();
+    }
+    @Override
+    protected void onPause() {
+        detener();
+        super.onPause();
+    }
+    private void acvtivarSensorAcelerometro(){
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        sensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        if(sensor==null){
+            tempVal.setText("Tu dispositivo NO cuenta con el sensor acelerometro.");
+            finish();
+        }
+        sensorEventListener = new SensorEventListener() {
+            @Override
+            public void onSensorChanged(SensorEvent sensorEvent) {
+                tempVal.setText("Acelerometro: X="+ sensorEvent.values[0]+ "; Y="+ sensorEvent.values[1] +"; Z="+
+                        sensorEvent.values[2]);
+            }
+            @Override
+            public void onAccuracyChanged(Sensor sensor, int i) {
+
+            }
+        };
+    }
+    private void inicar(){
+        sensorManager.registerListener(sensorEventListener, sensor,2000*1000 );
+    }
+    private void detener(){
+        sensorManager.unregisterListener(sensorEventListener);
+    }
+}
