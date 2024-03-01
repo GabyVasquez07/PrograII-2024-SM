5afb25bd6ac1e226b842e678f144e3b7abd14b15
GabyVasquez07 <gabrielaestefanivasquez@gmail.com>
2024-03-01 10:05:49 -0600

    actualizacion

--- app/src/main/java/com/ugb/controlesbasicos/MainActivity.java
+++ app/src/main/java/com/ugb/controlesbasicos/MainActivity.java
@@ -22,14 +22,48 @@ import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
-    TabHost tbh;
     TextView tempVal;
-    Spinner spn;
     Button btn;
+    String accion = "nuevo";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         btn = findViewById(R.id.btnGuardarAgendaAmigos);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    tempVal = findViewById(R.id.txtnombre);
+                    String nombre = tempVal.getText().toString();
+
+                    tempVal= findViewById(R.id.txtdireccion);
+                    String direccion = tempVal.getText().toString();
+
+                    tempVal = findViewById(R.id.txtTelefono);
+                    String telefono = tempVal.getText().toString();
+
+                    tempVal = findViewById(R.id.txtemail);
+                    String email = tempVal.getText().toString();
+
+                    tempVal = findViewById(R.id.txtdui);
+                    String dui = tempVal.getText().toString();
+
+                    DB db = new DB(getApplicationContext(),"", null,1);
+                    String[] datos = new String[]{"", nombre, direccion, telefono, email, dui};
+                    String respuesta = db.administrar_amigos(accion, datos);
+                    if(respuesta.equals("ok")){
+                        Toast.makeText(getApplicationContext(), "Amigo guardado con exito", Toast.LENGTH_LONG).show();
+                    }else{
+                        Toast.makeText(getApplicationContext(), "Error al intentar guardar el amigo:" + respuesta, Toast.LENGTH_LONG).show();
+                    }
+
+
+                }catch (Exception e){
+                    Toast.makeText(getApplicationContext(), "Error:" + e.getMessage(),Toast.LENGTH_LONG).show();
+                }
+            }
+        });
  }
 }
